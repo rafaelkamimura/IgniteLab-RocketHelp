e899b1a46382a001bd509de79b8ee08242972276
Rafael <rafael.kamimura@gmail.com>
2022-07-25 16:27:02 -0400

    inicialização

---
+++ README.md
@@ -0,0 +1,16 @@
+# IgniteLab-RocketHelp
+## Inicialização:
+```bash
+git clone https://github.com/rafaelkamimura/IgniteLab-RocketHelp.git
+cd IgniteLab-RocketHelp
+yarn add global expo-cli
+yarn add native-base
+yarn install
+expo install
+
+
+```
+
+## Download Google-Service API Key:
+
+https://console.firebase.google.com/project/rockethelp-f21f3/settings/general/
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,71 @@
+import { Heading, VStack, Icon, useTheme } from 'native-base';
+import { Envelope, Key } from 'phosphor-react-native';
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
+import { useState } from 'react';
+import Logo from '../assets/logo_primary.svg';
+import auth from '@react-native-firebase/auth';
+import { Alert } from 'react-native';
+
+export function SignIn() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const { colors } = useTheme();
+    function handleSignIn() {
+        if(!email || !password) {
+            return Alert.alert('Entrar', 'Informe e-mail e senha.');
+        }
+        setIsLoading(true);
+        auth()
+        .signInWithEmailAndPassword(email, password)
+        .catch((error) => {
+            console.log(error);
+            console.log(error.code)
+            setIsLoading(false);
+
+            if (error.code === 'auth/invalid-email') { 
+                return Alert.alert('Entrar', 'E-mail inválido.');
+            }
+
+            if (error.code === 'auth/wrong-password') { 
+                return Alert.alert('Entrar', 'E-mail ou senha inválida.');
+            }
+
+            if (error.code === 'auth/user-not-found') { 
+                return Alert.alert('Entrar', 'E-mail ou senha inválida.');
+            }
+
+            return Alert.alert('Entrar', 'Não foi possível acessar, tente novamente mais tarde.')
+        });
+    }
+    return (
+        <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24} >
+            <Logo />
+            <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+                Acesse sua conta
+            </Heading>
+
+            <Input 
+                mb={4}
+                placeholder="E-mail"
+                InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+                onChangeText={setEmail}
+            />
+            <Input
+                mb={8} 
+                placeholder="Senha"                
+                InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+                secureTextEntry
+                onChangeText={setPassword}
+            />
+            <Button 
+                title="Entrar"
+                w="full"
+                onPress={handleSignIn}
+                isLoading={isLoading}
+            />
+
+        </VStack>
+    )
+}
